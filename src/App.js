ab9fed3dcab11458126b810a2eabf1c622ca3b20
Bartek <bartek@macbook-pro-bartek.home>
2020-02-09 18:35:37 +0100

    react-router-dom beginning

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage';
+
+const HatsPage = () => (
+  <div>
+      <h1>Hats page</h1>
+  </div>
+);
+
+const TopicDetail = props => {
+  console.log(props);
+
+  return (
+    <div>
+      <h1>TOPIC DETAILS: {props.match.params.topicId}</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-        <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/topic/:topicId' component={TopicDetail} />
+      </Switch>
     </div>
   );
 }
