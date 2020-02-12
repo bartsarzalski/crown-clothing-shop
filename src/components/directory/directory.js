729cdc254b020d5224df9bef0b706b2cf052387d
Bartek <bartek@macbook-pro-bartek.home>
2020-02-12 09:20:13 +0100

    updated menu-item with withRouter

--- src/components/directory/directory.js
+++ src/components/directory/directory.js
@@ -13,7 +13,7 @@ class Directory extends Component {
                   title: 'hats',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
-                  linkUrl: 'shop/hats'
+                  linkUrl: 'shop/hats',
                 },
                 {
                   title: 'jackets',
@@ -49,12 +49,19 @@ class Directory extends Component {
         return(
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({ title, imageUrl, id, size }) => (
+                    /*this.state.sections.map(({ title, imageUrl, id, size, linkUrl }) => (
                         <MenuItem 
                             key={id} 
                             title={title} 
                             imageUrl={imageUrl}
-                            size={size} />
+                            size={size}
+                            linkUrl={linkUrl} />
+                    )) }*/
+
+                    //ES6 desctruction in diffrent way
+
+                    this.state.sections.map(({id, ...otherSectionsProps}) => (
+                        <MenuItem key={id} {...otherSectionsProps} />
                     ))
                 }
             </div>
