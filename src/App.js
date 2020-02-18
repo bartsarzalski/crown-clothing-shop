09d3e8a80e2da64eb1cc64fa3b7a0f921574ff98
Bartek <bartek@macbook-pro-bartek.home>
2020-02-18 21:59:35 +0100

    styling custom button and signIn component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 import Header from './components/header/header';
+import SignInAndSignUp from './pages/sing-in-and-sign-up/sign-in-and-sign-up';
 
 /* const HatsPage = () => (
   <div>
@@ -29,7 +30,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
